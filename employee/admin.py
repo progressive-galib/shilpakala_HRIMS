6c2e8dd853bf8872e975ef9d842b0d51570212d9
Hasan Mahmud <2hasanmahmud@gmail.com>
2024-04-26 21:20:43 +0600

    Office models done

--- employee/admin.py
+++ employee/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Employee
+from .models import Employee, Leave, ACR
 
 # Register your models here.
 class EmployeeList(admin.ModelAdmin):
   list_display = ("firstname", "lastname",)
 
 admin.site.register(Employee, EmployeeList)
+admin.site.register(Leave)
+admin.site.register(ACR)
--- employee/views.py
+++ employee/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Employee
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
-
+@csrf_exempt
 def employee(request):
 	if request.method == "GET":
 		employee = Employee.objects.all().values()
 		template = loader.get_template('employee.html')
 		context = {'employee':employee,
-		}
+			 }
 		return HttpResponse(template.render(context, request))
 
-	if request.method == "POST":
-		data = request.POST
-		return data
+#	elif request.method == "POST":
+#		data = json.loads(request.POST)
+#		print(request)
+#		return HttpResponse(data.get("firstname"))
